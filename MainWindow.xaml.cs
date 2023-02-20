66a0aedb36e27c6ebb74d5fef3e0ebb7d6b45d17
nyirkos <66072829+nyirkos@users.noreply.github.com>
2023-02-21 00:37:03 +0100

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
+using System.Windows;
+using System.Windows.Input;
 
 namespace WpfApp1
 {
@@ -23,9 +12,7 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         string adress = "https://learn.microsoft.com/";
-
-        
-
+        readonly string fileName = "links.txt";
 
         public MainWindow()
         {
@@ -35,70 +22,55 @@ namespace WpfApp1
 
         private void ButtonAddLink_Click(object sender, RoutedEventArgs e)
         {
-            
-
             if (!string.IsNullOrWhiteSpace(Linkstxt.Text) && !linksAdress.Items.Contains(Linkstxt.Text))
             {
-             
-
                 linksAdress.Items.Add(Linkstxt.Text);
-              
+
                 File.AppendAllText("links.txt", Linkstxt.Text + Environment.NewLine);
-              
 
-               
-               
-                
                 Linkstxt.Clear();
-
             }
-           
-
-    }
+        }
 
         // SelectItem method can open the link by doubleclick on it
         // First make adress eaqual with the selected item in the listbox ( inside the listbox there is the textbox => casting into string)
-        // Process.Start is just open the browser
+        // Process.Start can just open the browser
         private void SelectItem(object sender, MouseButtonEventArgs e)
         {
             adress = (string)linksAdress.SelectedItem;
             Process.Start("chrome.exe", $"{adress}");
         }
 
-        
-
         private void Remove_Click(object sender, RoutedEventArgs e)
         {
-            if(linksAdress.SelectedItem != null)
+            if (linksAdress.SelectedItem != null)
             {
 
-            linksAdress.Items.RemoveAt(linksAdress.Items.IndexOf(linksAdress.SelectedItem));
+                linksAdress.Items.RemoveAt(linksAdress.Items.IndexOf(linksAdress.SelectedItem));
 
-            StreamWriter SaveFile = new StreamWriter("links.txt");
-            foreach (var item in linksAdress.Items)
-            {
-                SaveFile.WriteLine(item);
-            }
+                StreamWriter SaveFile = new StreamWriter("links.txt");
+                foreach (var item in linksAdress.Items)
+                {
+                    SaveFile.WriteLine(item);
+                }
 
-            SaveFile.Close();
+                SaveFile.Close();
             }
 
         }
 
         private void Load()
         {
-            if ("links.text" != null)
+            if (!File.Exists(fileName))
             {
+              using (StreamWriter sw = File.CreateText("links.txt"));
+            }
 
-                string[] lines = File.ReadAllLines("links.txt");
-                foreach (var item in lines)
-                {
-                    linksAdress.Items.Add(item);
-
-                }
+            string[] lines = File.ReadAllLines("links.txt");
+            foreach (var item in lines)
+            {
+              linksAdress.Items.Add(item);
             }
         }
     }
-
-   
 }
